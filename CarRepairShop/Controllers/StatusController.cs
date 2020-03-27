116a5b6b303edad01882446f06edb7ad569d5292
Full Stack Coder Camp <fullstackcodercamp@gmail.com>
2020-03-27 19:31:31 -0400

    added complete order

---
+++ CarRepairShop/Controllers/StatusController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CarRepairShop.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarRepairShop.Controllers
+{
+    public class StatusController : Controller
+    {
+        public StatusController(CarRepairDbContext database)
+        {
+            Database = database;
+        }
+
+        public CarRepairDbContext Database { get; }
+
+
+        public IActionResult PendingOrders()
+        {
+            var orderStatus = Database.CustomerServices.Include(x => x.Customer).Where(x => x.Status == "InProgress").ToList();
+
+            return View(orderStatus);
+        }
+
+        public IActionResult CompletedOrders()
+        {
+            var orderStatus = Database.CustomerServices.Include(x => x.Customer).Where(x => x.Status != "InProgress").ToList();
+
+            return View(orderStatus);
+        }
+
+
+        public IActionResult CompleteOrder(Guid Id)
+        {
+            var order = Database.CustomerServices.FirstOrDefault(x => x.ServiceId == Id);
+            if(order == null)
+            {
+                return NotFound("No Order Found");
+            }
+            return View(order);
+        }
+
+        [HttpPost]
+        public IActionResult CompleteOrder(CustomerService customerService)
+        {
+            if (ModelState.IsValid)
+            {
+                if(customerService.TotalPaid < customerService.TotalCost)
+                {
+                    customerService.IsPaid = false;
+                    customerService.Status = "InProgress";
+                } else
+                {
+                    customerService.IsPaid = true;
+                    customerService.Status = "Completed";
+                }
+                Database.CustomerServices.Update(customerService);
+                Database.SaveChanges();
+                return RedirectToAction("PendingOrders");
+            }
+            return View(customerService);
+        }
+    }
+}
